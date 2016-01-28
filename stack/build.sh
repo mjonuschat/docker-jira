c7a9103c8d1b5b006439bb966ec987019a1a471d
Morton Jonuschat <mjonuschat@benzium-pro.mojocode.de>
2016-01-28 19:04:14 +0100

    [TASK] Update to Atlassian JIRA 7.0.10

--- stack/build.sh
+++ stack/build.sh
@@ -8,17 +8,23 @@ deb http://archive.ubuntu.com/ubuntu trusty-updates main
 deb http://archive.ubuntu.com/ubuntu trusty universe
 EOF
 
-JIRA_VERSION=6.4.11
+JIRA_VERSION=7.0.10
 
+# Install dependencies
 apt-get update
 apt-get install -y --force-yes \
     curl \
-    default-jre-headless \
     language-pack-de \
     language-pack-en \
     tar \
     zip \
     #
+
+# Download installer
+curl --silent \
+    --retry 3 \
+    --output /tmp/atlassian-jira-software-${JIRA_VERSION}-jira-${JIRA_VERSION}-x64.bin \
+    https://downloads.atlassian.com/software/jira/downloads/atlassian-jira-software-${JIRA_VERSION}-jira-${JIRA_VERSION}-x64.bin
 
 # Create a random user
 /usr/sbin/addgroup --quiet --gid 1000 jira
@@ -33,22 +39,40 @@ apt-get install -y --force-yes \
                   --home /home/jira \
                   jira
 
+# Prepare filesystem
 mkdir -p /srv/atlassian
 mkdir -p /srv/application-data/jira
 
-curl --silent --retry 3 http://downloads.atlassian.com/software/jira/downloads/atlassian-jira-${JIRA_VERSION}.tar.gz | tar -xz -C /srv/atlassian
+# Create unattended install response file
+cat >/tmp/response.varfile <<-RESPONSE
+https://raw.githubusercontent.com/docker-atlassian/jira/master/response.varfile
+#install4j response file for JIRA 7.0.10
+#Thu Jan 29 16:43:18 UTC 2016
+rmiPort$Long=8005
+app.jiraHome=/srv/application-data/jira
+app.install.service$Boolean=true
+existingInstallationDir=/srv/atlassian/jira
+sys.confirmedUpdateInstallationString=false
+sys.languageId=en
+sys.installationDir=/srv/atlassian/jira
+executeLauncherAction$Boolean=false
+httpPort$Long=8080
+portChoice=default
+RESPONSE
 
-ln -nsf /srv/atlassian/atlassian-jira-${JIRA_VERSION}-standalone /srv/atlassian/jira
+# Install application
+sh /tmp/atlassian-jira-software-${JIRA_VERSION}-jira-${JIRA_VERSION}-x64.bin -q -varfile /tmp/response.varfile
 
+# Forcibly set home directory
 cat >/srv/atlassian/jira/atlassian-jira/WEB-INF/classes/jira-application.properties <<-CONFIG
 # Do not modify this file unless instructed. It is here to store the location of the JIRA home directory only and is typically written to by the installer.
 jira.home = /srv/application-data/jira/
 CONFIG
 
-sed -i -e 's/<Connector port="8080"/<Connector port="5000"/' /srv/atlassian/jira/conf/server.xml
+# Fix permissions
+chown -R jira:jira /srv/atlassian/jira /srv/application-data/jira
 
-chown -R jira:jira /srv/atlassian/atlassian-jira-${JIRA_VERSION}-standalone /srv/atlassian/jira /srv/application-data/jira
-
+# Perform cleanup
 cd /
 rm -rf /var/cache/apt/archives/*.deb
 rm -rf /root/*
